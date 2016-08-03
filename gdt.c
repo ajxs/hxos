294c3f3bfa841785adfc75db8848364cdc7832c4
Anthony Richardson <anthony.richardson@optus.com.au>
2016-08-03 16:58:45 +1000

    fsafs

--- gdt.c
+++ gdt.c
@@ -6,8 +6,6 @@
 gdt_descr_t gdt[GDT_TABLE_SIZE];
 gdt_ptr_t gp;
 
-extern void _gdt_flush(void);
-
 void gdt_set_gate(uint8_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags) {
 	gdt[num].limit_low = (limit & 0xFFFF);
 	gdt[num].base_low = (base & 0xFFFF);
@@ -22,15 +20,12 @@ void gdt_set_gate(uint8_t num, uint32_t base, uint32_t limit, uint8_t access, ui
 
 
 void gdt_install(void) {
-
 	gp.limit = (uint16_t)(sizeof(gdt_descr_t) * GDT_TABLE_SIZE) - 1;
-	gp.base = (uint32_t)&gdt - KERNEL_VIRTUAL_BASE;
-
+	gp.base = ((uint32_t)&gdt) - KERNEL_VIRTUAL_BASE;
 
 	gdt_set_gate(0, 0, 0, 0, 0);		// NULL descriptor
 	gdt_set_gate(1, 0, 0xFFFFFFFF, GDT_ACCESS_CODE_PL0, GDT_FLAGS);
 	gdt_set_gate(2, 0, 0xFFFFFFFF, GDT_ACCESS_DATA_PL0, GDT_FLAGS);
 	gdt_set_gate(3, 0, 0xFFFFFFFF, GDT_ACCESS_CODE_PL3, GDT_FLAGS);
 	gdt_set_gate(4, 0, 0xFFFFFFFF, GDT_ACCESS_DATA_PL3, GDT_FLAGS);
-	//_gdt_flush();
 };
